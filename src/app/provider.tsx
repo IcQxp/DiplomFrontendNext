c689eaa2e405371afd72e1199fbf2f3d97cbeaa0
IcQxp <batla3080@gmail.com>
2025-06-21 23:10:44 +0300

    page + layout

---
+++ src/app/provider.tsx
@@ -0,0 +1,5 @@
+'use client'
+import store from '@/store'
+import { Provider } from 'react-redux'
+
+export function ReduxToolkitProvider({children,}: {children: React.ReactNode}) {return <Provider store={store}>{children}</Provider>}
