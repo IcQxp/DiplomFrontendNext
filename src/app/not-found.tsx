80ce814dbbae8d59b527ffb6fd40bf49ac0faf6f
IcQxp <batla3080@gmail.com>
2025-06-22 23:26:53 +0300

    simples routes

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+/**
+ * A function component that renders a 404 error page for a post.
+ * @returns {JSX.Element} A JSX element representing the 404 error page.
+ */
+export default function PostNotFound() {
+  return (
+    <div className='container'>
+      <p>Такой страницы не существует</p>
+    </div>
+  )
+}
